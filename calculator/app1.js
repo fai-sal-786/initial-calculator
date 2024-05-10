1a5f6ae60fc01ff7093852a9ec2bd166c60cf8e4
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-05-10 01:57:36 -0700

    initial calculator

---
+++ calculator/app1.js
@@ -0,0 +1,36 @@
+var input = document.getElementById('num');
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+var result = document.getElementById('result');
+
+
+
+
+
+
+function isEven(num) {
+    if(num % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function checkNumber() {
+    if(isEven(input.value)){
+        result.innerText = "Number is Even";
+    } else{
+        result.innerText = "Number is Odd";
+    }
+}
+
+function calculate(sign = '+') {
+ var num1Val = num1.value;
+ var num2Val = num2.value;
+
+ if(sign == '+') return(result.innerText = parseInt(num1Val) + parseInt(num2Val));
+ if(sign == '-') return(result.innerText = num1Val - num2Val);
+ if(sign == '*') return(result.innerText = num1Val * num2Val);
+ if(sign == '/') return(result.innerText = num1Val / num2Val);
+ }
